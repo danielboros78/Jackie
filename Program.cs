336546be44340e84200d10a3c6465ab0d1136658
danielboros78 <danielboros78@gmail.com>
2021-09-13 10:51:34 +0200

    Kiindulási állapot

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Jackie
 {
@@ -10,6 +11,42 @@ namespace Jackie
     {
         static void Main(string[] args)
         {
+            ElsoFeladat();
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            Otodikfeladat();
+            HatodikFeladat();
+        }
+
+        private static void HatodikFeladat()
+        {
+
+        }
+
+        private static void Otodikfeladat()
+        {
+
+        }
+
+        private static void NegyedikFeladat()
+        {
+
+        }
+
+        private static void HarmadikFeladat()
+        {
+
+        }
+
+        private static void MasodikFeladat()
+        {
+
+        }
+
+        private static void ElsoFeladat()
+        {
+
         }
     }
 }
